a46bf7d805f191443ef560bfb99b80c5fdc4ad78
anikeieva <alyonaoooooooo@gmail.com>
2018-06-18 14:29:35 +0300

    Added readline-sync and greeting of user

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const greeting = () => {
+  const readlineSync = require('readline-sync');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+export default greeting;
