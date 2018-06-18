df7863060d23e4e2eaf77dec2191c0ee9c211e29
anikeieva <alyonaoooooooo@gmail.com>
2018-06-18 18:20:46 +0300

    added codeclimate, eslint and travis-ci. Also fixed last steps: added discription and removed fild main to package.json, added .log files to .gitignore and changed require to import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const greeting = () => {
-  const readlineSync = require('readline-sync');
+import readlineSync from 'readline-sync';
 
+const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
