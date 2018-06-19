70f5b6e152570b5c4d70aa17ebb78fca653ec525
anikeieva <alyonaoooooooo@gmail.com>
2018-06-19 12:15:39 +0300

    Done 4-th step: added new game - bran-even. Users must type yes if number even and no if else. Published apdates in npm and added folder dist to .eslintignore

---
+++ src/bin/brain-even.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { greeting, getEvenOrNot } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+
+console.log('');
+greeting();
+getEvenOrNot();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
+let userName;
+
+export const greeting = () => {
+  userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
-export default greeting;
+
+export const getEvenOrNot = () => {
+  const getRandomNumber = (min, max) => {
+    const result = (Math.random() * (max - min)) + min;
+    return result;
+  };
+  let counter = 0;
+  const maxIter = 3;
+
+  console.log('');
+
+  while (counter !== maxIter) {
+    const randomNumber = Math.round(getRandomNumber(1, 100));
+
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const getRightAnswer = () => {
+      if (randomNumber % 2 === 0) {
+        return 'yes';
+      }
+      return 'no';
+    };
+    const rightAnswer = getRightAnswer();
+
+    if (answer === rightAnswer) {
+      console.log('Correct!');
+      counter++;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+  }
+
+  if (counter === maxIter) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
