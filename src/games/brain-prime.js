2652f8084f7c4e7b538d041b05ddf62fd45f50f9
anikeieva <alyonaoooooooo@gmail.com>
2018-06-21 22:21:41 +0300

    simplified function for getting is number prime or not

---
+++ src/games/brain-prime.js
@@ -0,0 +1,36 @@
+import startNewGame from '..';
+import getRoundRandomNumber from '../untils';
+
+const gameDiscription = 'Is this number prime?';
+
+const isPrime = (number) => {
+  let result;
+  if (number === 1) {
+    return false;
+  } else if (number === 2) {
+    return true;
+  }
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const getRightAnswer = n => (isPrime(n) ? 'yes' : 'no');
+
+const getGameData = () => {
+  const question = getRoundRandomNumber(1, 100);
+  const answer = getRightAnswer(question);
+
+  const gameData = {
+    question,
+    answer,
+  };
+  return gameData;
+};
+
+const runGame = () => startNewGame(gameDiscription, getGameData);
+export default runGame;
